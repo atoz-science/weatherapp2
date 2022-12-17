f45ba956dd078cfbc47879007c669a6e48b88d1c
Ghulam Ali <alisam9697@gmail.com>
2022-12-17 18:13:23 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+// import UilReact from "@iconscout/react-unicons/icons/uil-react";
+import "./App.css";
+import Forecast from "./components/Forecast";
+import Inputs from "./components/Inputs";
+import TemperatureAndDetails from "./components/TemperatureAndDetails";
+import TimeandLocation from "./components/TimeandLocation";
+import TopButtons from "./components/TopButtons";
+import getFormattedWeatherData from "./services/weatherServices";
 
 function App() {
+
+  const fetchWeather = async () => {
+    const data = await getFormattedWeatherData({ q: "london" });
+    console.log(data);
+  };
+
+  fetchWeather();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+      <TopButtons />
+      <Inputs />
+      
+      <TimeandLocation />
+      <TemperatureAndDetails />
+      <Forecast title="hourly forecast"/>
+      <Forecast title="daily forecast"/>
+  </div>
   );
 }
 
---
+++ src/components/TimeandLocation.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function TimeandLocation() {
+  return (
+    <div>
+      <div className="flex justify-center items-center my-6">
+        <p className="text-xl font-extralight text-white">
+          Thursday, 04 August 2022 | Local Time 13:45
+        </p>
+      </div>
+      <div className="flex items-center justify-center my-3">
+        <p className="font-medium text-3xl text-white">Berlin, DE</p>
+      </div>
+    </div>
+  );
+}
+
+export default TimeandLocation;
